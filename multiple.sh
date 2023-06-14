83695386f4579eaf54d4834bf77cb6b7fb3e50a0
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-06-14 10:48:04 +0530

    Update multiple.sh

--- multiple.sh
+++ multiple.sh
@@ -8,3 +8,18 @@ for ((i = 1; i <= 10; i++)); do
     result=$((number * i))
     echo "$number x $i = $result"
 done
+////output////
+mca@cscc2d78:~$ bash multiple.sh
+Enter a number:
+5
+Multiplication table for 5:
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
